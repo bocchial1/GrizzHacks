cac2228125d418be3cb9a1bf784b0c10f2186e6e
Alessandro Bocchi <119146496+bocchial1@users.noreply.github.com>
2024-03-09 14:52:52 -0500

    ui updates

--- Helping-Hands/src/app/login/login.component.ts
+++ Helping-Hands/src/app/login/login.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onSubmit() {
+    // Add your login logic here
+    // For simplicity, we'll just log the credentials to the console
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
