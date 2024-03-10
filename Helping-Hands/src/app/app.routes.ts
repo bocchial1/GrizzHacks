16a2c8fd42bb0d566a76b31aa3911d3acb3ae327
Alessandro Bocchi <119146496+bocchial1@users.noreply.github.com>
2024-03-09 20:54:14 -0500

    NEW

--- Helping-Hands/src/app/app.routes.ts
+++ Helping-Hands/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { PackPalScreenComponent } from './pack-pal-screen/pack-pal-screen.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
   {
@@ -17,21 +17,8 @@ export const routes: Routes = [
     path: 'PackPals',
     component: PackPalScreenComponent,
   },
-=======
-import { SignupComponent } from './signup/signup.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent, 
-    },
-    {
-        path: 'Login',
-        component: LoginComponent, 
-    },
     {
         path: 'Signup',
         component: SignupComponent, 
     },
->>>>>>> 140757da8d7aecb30d5d9fad38977d8a8ddee7a5
 ];
