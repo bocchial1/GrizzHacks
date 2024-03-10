8f3f1c82efb0cc80d64c10088db2e8e6b3f0423c
Alessandro Bocchi <119146496+bocchial1@users.noreply.github.com>
2024-03-09 19:10:41 -0500

    new

--- Helping-Hands/src/app/app.routes.ts
+++ Helping-Hands/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'Login',
         component: LoginComponent, 
+    },
+    {
+        path: 'Signup',
+        component: SignupComponent, 
     },
 ];
