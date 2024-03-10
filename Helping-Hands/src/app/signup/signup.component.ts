140757da8d7aecb30d5d9fad38977d8a8ddee7a5
Alessandro Bocchi <119146496+bocchial1@users.noreply.github.com>
2024-03-09 20:40:32 -0500

    new

--- Helping-Hands/src/app/signup/signup.component.ts
+++ Helping-Hands/src/app/signup/signup.component.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
+  imports: [FormsModule],
   styleUrls: ['./signup.component.css'],
   standalone: true
 })
 export class SignupComponent {
-  username: string = '';
-  password: string = '';
-
-  constructor(private userService: UserService) {}
-
-  signUp(): void {
-    const newUser = { username: this.username, password: this.password };
-    this.userService.addUser(newUser).subscribe(() => {
-      console.log('User added successfully!');
-    });
-  }
+  
 }
